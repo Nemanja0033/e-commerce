f658669faad8cb201308636fe73969e7ab077ce0
nemanja <antonijevicnemanja68@gmail.com>
2024-11-07 12:27:47 +0100

    colors adjustment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,21 +3,21 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav className="w-full bg-primary  h-[50px] flex justify-between items-center">
+    <nav className="w-full bg-gray-50  h-[50px] flex justify-between items-center">
         <div className="md:ml-12 ml-8">
             <Link to={'/'}>
-                <h1 className="md:text-3xl text-xl text-white cursor-pointer">m o d e.</h1>
+                <h1 className="md:text-3xl text-xl text-primary cursor-pointer">m o d e.</h1>
             </Link>
         </div>
         <div className="md:mr-12 mr-8 flex gap-8">
             <Link to={'/products'}>
-                <span className="text-white md:text-md text-sm">Products</span>
+                <span className="text-primary md:text-md text-sm">Products</span>
             </Link>
             <Link to={'/cart'}>
-            <ShoppingBag className="cursor-pointer" color="white" />
+            <ShoppingBag className="cursor-pointer" color="black" />
             </Link>
             <Link to={'/register'}>
-                <UserIcon  className="cursor-pointer" color="white" />
+                <UserIcon  className="cursor-pointer" color="black" />
             </Link> 
         </div>
     </nav>

0d1fc13c446f7a5923738dadf1414a4adc5d6348
nemanja <antonijevicnemanja68@gmail.com>
2024-11-08 13:06:45 +0100

    helper funciton

---
+++ src/helpers/useFetch.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url: string) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+        const data = await response.json();
+        setData(data);
+      }
+      catch (err){
+        console.log('Error while fethcing', err)
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data };
+};
+
+export default useFetch;

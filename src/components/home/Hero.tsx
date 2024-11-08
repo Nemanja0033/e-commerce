787ca6aa5836fba72d4a6c2bca59632491c78d8c
nemanja <antonijevicnemanja68@gmail.com>
2024-11-08 13:20:12 +0100

    custom hooks

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,22 +1,36 @@
-import { Button } from "@mui/material"
+import { Button } from "@mui/material";
+import useImagesChanger from "../../custom hooks/useImagechanger";
 
 const Hero = () => {
+    
+    const { images, imageIndex } = useImagesChanger();
 
   return (
     <div className="md:flex flex-row justify-center items-center w-full gap-8">
-        <div className="flex-row md:mt-0  text-center">
-                <div className="flex justify-center items-center m-6 md:m-0">
-                <h1 className="text-4xl text-primary">Discover the Art of Fashion with <span className="font-semibold">m o d e</span></h1>
-                </div>
-                <div className="flex justify-center items-center mt-3">
-                    <Button variant="outlined"  color="secondary" size="small">Explore Collection</Button>
-                </div>
+      <div className="flex-row md:mt-0 text-center">
+        <div className="flex justify-center items-center m-6 md:m-0">
+          <h1 className="text-4xl text-primary">
+            Discover the Art of Fashion with <span className="font-semibold">m o d e</span>
+          </h1>
         </div>
-        <div className="mt-8 flex justify-center">
-            <img className="h-[550px] rounded-xl" src="https://images.pexels.com/photos/15376545/pexels-photo-15376545/free-photo-of-elegant-woman-wearing-sunglasses-and-handbag.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-        </div> 
+        <div className="flex justify-center items-center mt-3">
+          <Button variant="outlined" color="secondary" size="small">
+            Explore Collection
+          </Button>
+        </div>
+      </div>
+      <div className="mt-8 flex justify-center relative h-[550px] w-[400px]">
+        {images.map((img, i) => (
+          <img
+            key={i}
+            className={`h-full w-full rounded-xl object-cover absolute transition-opacity duration-1000 ease-in-out ${i === imageIndex ? 'opacity-100' : 'opacity-0'}`}
+            src={img}
+            alt={`Fashionable ${i}`}
+          />
+        ))}
+      </div>
     </div>
-   )
-}
+  );
+};
 
-export default Hero
+export default Hero;

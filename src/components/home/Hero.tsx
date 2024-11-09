b69cef653ce5d5c9fc5c6a74696170f703959549
nemanja <antonijevicnemanja68@gmail.com>
2024-11-09 13:09:54 +0100

    hero video

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,34 +1,30 @@
 import { ArrowDown } from "lucide-react";
-import useImagesChanger from "../../custom hooks/useImagechanger";
 
 const Hero = () => {
-    const { images, imageIndex } = useImagesChanger();
-
   return (
-    <div className="md:flex flex-row justify-center items-center w-full shadow-md mt-12 ">
-      <div className="flex-row md:mt-0 text-center ml-auto">
-        <div className="flex justify-center items-center md:m-0 mt-24 md:mt-0">
-          <h1 className="md:text-5xl text-3xl text-primary text-center">
-            Discover the Art of Fashion with <span className="font-semibold">m o d e</span>
-          </h1>
-        </div>
-        <div className="mt-3">
-            <p className="text-gray-400 md:text-xl text-md tracking-widest text-center">`New collecion is out now` </p>
-        </div>
+    <div className="relative flex justify-center items-center w-full shadow-md h-screen">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+        autoPlay
+        loop
+        muted
+      >
+        <source src="/videos/video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <div className="text-center relative z-10">
+        <h1 className="md:text-5xl text-3xl text-white">
+          Discover the Art of Fashion with <span className="font-semibold">m o d e</span>
+        </h1>
+        <p className="text-gray-400 md:text-xl text-md tracking-widest mt-3">
+          New collection is out now
+        </p>
         <div className="flex justify-center items-center mt-6">
-          <a href="#featured" className="text-xl flex items-center">Explore Collections <ArrowDown /></a>
+          <a href="#featured" className="text-xl flex items-center text-white">
+            Explore Collections <ArrowDown />
+          </a>
         </div>
-       
-      </div>
-      <div className= "md:mt-12 mt-20  flex justify-center relative m-auto h-[450px] w-[300px] mb-12">
-        {images.map((img: any, i) => (
-          <img
-            key={i}
-            className={`h-full w-full object-cover absolute transition-opacity duration-1000 ease-in-out ${i === imageIndex ? 'opacity-100' : 'opacity-0'}`}
-            src={img}
-            alt={`Fashionable ${i}`}
-          />
-        ))}
       </div>
     </div>
   );

1d318b7c10eb36bc0183231d04a612897791f7ae
nemanja <antonijevicnemanja68@gmail.com>
2024-11-11 14:39:49 +0100

    Social Links section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import Footer from "../components/Footer"
 import FeaturedProducts from "../components/home/FeaturedProducts"
 import Hero from "../components/home/Hero"
 import ImageSlider from "../components/home/ImageSlider"
+import SocialLinks from "../components/home/SocialLinks"
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <FeaturedProducts />
       <ImageSlider />
+      <SocialLinks />
       <Footer />
     </div>
   )
